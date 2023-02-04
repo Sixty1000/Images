c5e9e6415105b60e32959d88771858a7f1262d6d
n3ha1105 <85949085+n3ha1105@users.noreply.github.com>
2023-02-04 14:24:13 -0800

    Add files via upload
    Neha's methods
    - rotate images
    - convert to old fashioned
    - photo negative

--- ImageProcessing.java
+++ ImageProcessing.java
@@ -10,8 +10,14 @@ public class ImageProcessing{
 		APImage image4 = new APImage("smokey.jpg");
 		
 		APImage edgeImage = EdgeDetection(image4, 20);
+		APImage rotatedLeft = rotate90Left(image3);
+		APImage rotatedRight = rotate90Right(image4);
+		APImage rotated = rotate180(image2);
 		
 		edgeImage.draw();
+		rotatedLeft.draw();
+		rotatedRight.draw();
+		rotated.draw();
 		
 	}
 	
@@ -81,6 +87,52 @@ public class ImageProcessing{
 		
 		return sketch;
 	}
+	
+	/**
+	 * 
+	 * @param img APImage that will be rotated left 90º
+	 * @return rotated APImage (does not change the original image
+	 */
+	public static APImage rotate90Left(APImage img) {
+		int width = img.getHeight();
+		int height = img.getWidth();
+		APImage sketch = new APImage(width, height);
+		
+		for(int i = 0; i<img.getHeight(); i++) {
+			for(int j = 0; j<img.getWidth(); j++) {
+				//pixel at position [i][j] goes to pixel at position [height-1-j][i]
+				sketch.setPixel(i, height-1-j, img.getPixel(j,i));
+			}
+		}
+		return sketch;
+	}
+	public static APImage rotate90Right(APImage img) {
+		int width = img.getHeight();
+		int height = img.getWidth();
+		APImage sketch = new APImage(width, height);
+		
+		for(int i = 0; i<img.getHeight(); i++) {
+			for(int j = 0; j<img.getWidth(); j++) {
+				//pixel at position [i][j] goes to pixel at position [height-1-j][i]
+				sketch.setPixel(img.getHeight()-1-i, j, img.getPixel(j,i));
+			}
+		}
+		return sketch;
+	}
+	public static APImage rotate180(APImage img) {
+		int width = img.getWidth();
+		int height = img.getHeight();
+		APImage sketch = new APImage(width, height);
+		
+		for(int i = 0; i<height; i++) {
+			for(int j = 0; j<width; j++) {
+				//pixel at position [i][j] goes to pixel at position [height-1-j][i]
+				sketch.setPixel(width-1-j, height-1-i, img.getPixel(j,i));
+			}
+		}
+		return sketch;
+	}
+	
 	/*
 	public static APImage Shrink(APImage img, int fact){
 		int width = img.getWidth()/fact;
@@ -102,3 +154,70 @@ public class ImageProcessing{
 		return sketch;
 	}*/
 }
+
+
+
+
+
+/*import images.APImage;
+import images.Pixel;
+
+public class ImageProcessing{
+	public static void main(String[] args){
+		APImage butterfly = new APImage("butterfly1.jpg");
+		APImage koala = new APImage("koala.jpg");
+		//APImage arch = new APImage("arch.jpg");
+		//APImage motor = new APImage("redMotercycle.jpg");
+		//APImage bird = new APImage("seagull.jpg");
+		//APImage cat = new APImage("smokey.jpg");
+		//APImage uglyDuck = new APImage("swan.jpg");
+		GrayScale(butterfly);
+		butterfly.draw();
+		BlackWhite(koala);
+		koala.draw();
+		
+		
+	}
+	
+	public static void GrayScale(APImage i) {
+		for(Pixel p: i) {
+			int r = p.getRed();
+			int g = p.getGreen();
+			int b = p.getBlue();
+			
+			int avgVal = (r+g+b)/3;
+			
+			r = avgVal;
+			g = avgVal;
+			b = avgVal;
+			
+			p.setRed(r);
+			p.setBlue(b);
+			p.setGreen(g);
+		}
+	}
+	
+	public static void BlackWhite(APImage i) {
+		for(Pixel p : i) {
+			
+			int r = p.getRed();
+			int g = p.getGreen();
+			int b = p.getBlue();
+			
+			int avgVal = (r+g+b)/3;
+			
+			if(avgVal < 128) {
+				p.setRed(0);
+				p.setGreen(0);
+				p.setBlue(0);
+			}else {
+				p.setRed(255);
+				p.setGreen(255);
+				p.setBlue(255);
+			}
+		}
+	}
+	
+	
+}
+*/
